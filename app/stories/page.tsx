f350a65b8e4cef50a4bdaceaea5756a9d8c550d8
Karim111171 <karim.keirouz@gmail.com>
2026-03-25 20:59:18 +0200

    pics and redirects

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -13,7 +13,33 @@ export default function StoriesPage() {
         </p>
 
         <div className="story-card">
-          <h2>1- La Communication</h2>
+          <h2>1- L'histoire de Lila et Kaia</h2>
+	    <Image src="/histoirelilakaiou.png" alt="histoire de Lila et Kaiou" width={280} height={220} />
+          <p>
+            Un petit album illustré de Lila & Kaiou à lire en ligne ou à télécharger.
+          </p>
+
+          <div className="story-actions">
+            <a
+              href="/histoires/histoirelilakaiou.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cta-button"
+            >
+              Lire le PDF
+            </a>
+
+            <a
+              href="/histoires/lila-et-kaiou-histoire-1.pdf"
+              download
+              className="cta-button secondary-button"
+            >
+              Télécharger
+            </a>
+          </div>
+        </div>
+	  <div className="story-card">
+          <h2>2- La Communication</h2>
 	    <Image src="/Snapshot.png" alt="Pâte magique 200g" width={280} height={220} />
           <p>
             Un petit album illustré de Lila & Kaiou à lire en ligne ou à télécharger.
@@ -22,6 +48,33 @@ export default function StoriesPage() {
           <div className="story-actions">
             <a
               href="/histoires/1 - La communication - Lila & Kaiou.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cta-button"
+            >
+              Lire le PDF
+            </a>
+
+            <a
+              href="/histoires/lila-et-kaiou-histoire-1.pdf"
+              download
+              className="cta-button secondary-button"
+            >
+              Télécharger
+            </a>
+          </div>
+        </div>
+
+	  <div className="story-card">
+          <h2>3- L'histoire de Kaiou</h2>
+	    <Image src="/Kaiou.png" alt="histoire de Lila et Kaiou" width={280} height={220} />
+          <p>
+            Un petit album illustré de Lila & Kaiou à lire en ligne ou à télécharger.
+          </p>
+
+          <div className="story-actions">
+            <a
+              href="/histoires/histoirelilakaiou.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="cta-button"
--- lib/products.ts
+++ lib/products.ts
@@ -10,8 +10,8 @@ export type Product = {
 export const PRODUCTS: Product[] = [
   {
     id: 1,
-    name: "Pâte à modeler naturelle Lila & Kaiou – 200g",
-    shortName: "Pâte à modeler 200g",
+    name: "Pâte à modeler naturelle Lila & Kaiou – 100g",
+    shortName: "Pâte à modeler 100g",
     price: 7.50,
     image: "/Pot200gr.png",
     description:
@@ -19,21 +19,84 @@ export const PRODUCTS: Product[] = [
   },
   {
     id: 2,
-    name: "Pâte magique Kaiou",
-    shortName: "Pâte magique Kaiou",
-    price: 9.00,
-    image: "/Kaiou.png",
-    description:
-      "Une version inspirée du personnage Kaiou pour enrichir le jeu, l’imagination et l’univers créatif des enfants.",
-  },
-  {
-    id: 3,
-    name: "Pâte à modeler naturelle Lila & Kaiou – 6 x 200g",
-    shortName: "Pâte magique 200g",
+    name: "Pâte à modeler naturelle Lila & Kaiou – 6 x 100g",
+    shortName: "Pâte à modeler 200g",
     price: 39.00,
     image: "/6pack_no_bg.png",
     description:
       "Une pâte à modeler naturelle, douce et créative inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 21,
+    name: "Stand de Pâte à modeler naturelle Lila & Kaiou – 36 x 100g",
+    shortName: "Pâte à modeler 200g",
+    price: 39.00,
+    image: "/36pack.png",
+    description:
+      "Une pâte à modeler naturelle, douce et créative inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 3,
+    name: "Magic Bucket  Lila & Kaiou – 600gr",
+    shortName: "Pâte magique 600g",
+    price: 45.00,
+    image: "/potmagic.png",
+    description:
+      "Une pâte à modeler naturelle, avec une activité inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 4,
+    name: "Poupée Kaiou – Lila & Kaiou – 17cm",
+    shortName: "Poupée Kaiou",
+    price: 24.00,
+    image: "/poupeekaiou.png",
+    description:
+      "La poupée Kaiou est le doudou préféré de Lila ! – Inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 5,
+    name: "Poupée Lila – Lila & Kaiou – 29cm",
+    shortName: "Poupée Lila",
+    price: 29.00,
+    image: "/poupeelila.png",
+    description:
+      "La poupée Lila est le doudou préféré de Kaia ! – Inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 6,
+    name: "Ciné Kaiou – Lila & Kaiou – 7cmx4cm",
+    shortName: "Stop Motion Kaiou",
+    price: 9.00,
+    image: "/Kaiou.png",
+    description:
+      "Carnets cartonnés pour réaliser des animations et stimuler la créativité chez l'enfant. Lila en crayons et Kaiou la gomme",
+  },
+   {
+    id: 7,
+    name: "Double Familly – Lila & Kaiou – 8cmx5cm",
+    shortName: "Stop Kaiou",
+    price: 19.00,
+    image: "/double-face.webp",
+    description:
+      "Jeux de carte éducatif et d'eveil pour enfants a partir de 5 ans. Créé par Kaia pour jouer avec sa famille adorée",
+  },
+  {
+    id: 8,
+    name: "Stop Kaiou – Lila & Kaiou – 12cm",
+    shortName: "Poupée Kaiou",
+    price: 29.00,
+    image: "/poupeekaiou.png",
+    description:
+      "Le stop porte le plus mignon ! Kaiou sert à tout et à tous ! – Inspirée de l’univers magique de Lila & Kaiou.",
+  },
+  {
+    id: 9,
+    name: "T-night – Lila & Kaiou – 12cm",
+    shortName: "T-shirt Kaiou",
+    price: 19.00,
+    image: "/t-night.png",
+    description:
+      "Le T-shirt pyjama Kaiou ! Vous ne dormirez pas seul ce soir! – Inspirée de l’univers magique de Lila & Kaiou.",
   },
 ];
 
